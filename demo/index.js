4ad9a29f5fe76acf4b45c94cb09a309bc25305d2
Sergej Sintschilin <seregpie@gmail.com>
2018-06-22 23:20:59 +0200

    +

--- demo/index.js
+++ demo/index.js
@@ -4,8 +4,28 @@
 		el: '#app',
 
 		components: {
-			MyExampleI0: {
-				template: '#app-example-i0',
+			MyExampleI1: {
+				template: '#app-example-i1',
+
+				data: function() {
+					return {
+						position: [1/2, 1/2],
+					};
+				},
+
+				methods: {
+					onDrag: function(point) {
+						var bounds = this.$refs.dragArea.getBoundingClientRect();
+						this.position = [
+							Math.min(Math.max((point[0] - bounds.left) / bounds.width, 0), 1),
+							Math.min(Math.max((point[1] - bounds.top) / bounds.height, 0), 1),
+						];
+					},
+				},
+			},
+
+			MyExampleI2: {
+				template: '#app-example-i2',
 			},
 		},
 	});
